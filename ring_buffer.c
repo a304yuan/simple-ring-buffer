2ced591f6a5562963788dc302cc15f00a91ee00c
a304yuan <a304yuanyuan@gmail.com>
2018-12-11 15:49:21 +0800

    change buffer mark type

--- ring_buffer.c
+++ ring_buffer.c
@@ -1,55 +1,60 @@
 #include "ring_buffer.h"
 
-ring_buffer * ring_buffer_new(size_t elem_size) {
+ring_buffer * ring_buffer_new(size_t elem_size, size_t capacity) {
     ring_buffer * buf = malloc(sizeof(ring_buffer));
     buf->elem_size = elem_size;
+    buf->capacity = capacity;
     atomic_init(&buf->read_cursor, 0);
     atomic_init(&buf->write_cursor, 0);
     atomic_init(&buf->count, 0);
-    for (int i = 0; i < DEFAULT_BUFFER_SIZE / 8; i++) {
+    buf->buf = malloc(capacity * elem_size);
+    buf->buf_mark = malloc(capacity * sizeof(int));
+    for (int i = 0; i < capacity; i++) {
         atomic_init(buf->buf_mark + i, 0);
     }
-    buf = malloc(DEFAULT_BUFFER_SIZE * elem_size);
     return buf;
 }
 
 void ring_buffer_free(ring_buffer * buf) {
     free(buf->buf);
+    free(buf->buf_mark);
     free(buf);
 }
 
 int ring_buffer_read(ring_buffer * buf, void * dest) {
-    long count = atomic_load(&buf->count);
+    long count = atomic_load_explicit(&buf->count, memory_order_relaxed);
     while (count > 0) {
-        atomic_compare_exchange_strong(&buf->count, &count, count - 1);
+        if (atomic_compare_exchange_strong(&buf->count, &count, count - 1)) {
+            break;
+        }
     }
     if (count == 0) {
         return 0;
     }
-    long idx = atomic_fetch_add(&buf->read_cursor, 1) % DEFAULT_BUFFER_SIZE;
-    _Atomic uint8_t * mark = buf->buf_mark + idx / 8;
+    long idx = atomic_fetch_add_explicit(&buf->read_cursor, 1, memory_order_acq_rel) % buf->capacity;
     // loop until ready to read
-    while (!(atomic_load(mark) & (1 << idx % 8)));
+    while (!atomic_load_explicit(buf->buf_mark + idx, memory_order_acquire));
     memcpy(dest, buf->buf + idx * buf->elem_size, buf->elem_size);
     // change state to ready-to-write
-    atomic_fetch_and(mark, ~(1 << idx % 8));
+    atomic_store_explicit(buf->buf_mark + idx, 0, memory_order_release);
     return 1;
 }
 
 int ring_buffer_write(ring_buffer * buf, void * src) {
-    long count = atomic_load(&buf->count);
-    while (count < DEFAULT_BUFFER_SIZE) {
-        atomic_compare_exchange_strong(&buf->count, &count, count + 1);
+    long count = atomic_load_explicit(&buf->count, memory_order_relaxed);
+    while (count < buf->capacity) {
+        if (atomic_compare_exchange_strong(&buf->count, &count, count + 1)) {
+            break;
+        }
     }
-    if (count == DEFAULT_BUFFER_SIZE) {
+    if (count == buf->capacity) {
         return 0;
     }
-    long idx = atomic_fetch_add(&buf->write_cursor, 1) % DEFAULT_BUFFER_SIZE;
-    _Atomic uint8_t * mark = buf->buf_mark + idx / 8;
+    long idx = atomic_fetch_add_explicit(&buf->write_cursor, 1, memory_order_acq_rel) % buf->capacity;
     // loop until ready to write
-    while (atomic_load(mark) & (1 << idx % 8));
+    while (atomic_load_explicit(buf->buf_mark + idx, memory_order_acquire));
     memcpy(buf->buf + idx * buf->elem_size, src, buf->elem_size);
     // change state to ready-to-read
-    atomic_fetch_or(mark, 1 << idx % 8);
+    atomic_store_explicit(buf->buf_mark + idx, 1, memory_order_release);
     return 1;
 }
--- ring_buffer.h
+++ ring_buffer.h
@@ -4,23 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdatomic.h>
-#include <stdint.h>
-
-#define DEFAULT_BUFFER_SIZE 1024
 
 typedef struct ring_buffer ring_buffer;
 
 struct ring_buffer {
     size_t elem_size;
+    size_t capacity;
     _Atomic long read_cursor;
     _Atomic long write_cursor;
     _Atomic long count;
     // mark: 0 - ready-to-write; 1 - ready-to-read
-    _Atomic uint8_t buf_mark[DEFAULT_BUFFER_SIZE/8];
+    _Atomic int *buf_mark;
     void * buf;
 };
 
-extern ring_buffer * ring_buffer_new(size_t elem_size);
+extern ring_buffer * ring_buffer_new(size_t elem_size, size_t capacity);
 extern void ring_buffer_free(ring_buffer * buf);
 extern int ring_buffer_read(ring_buffer * buf, void * dest);
 extern int ring_buffer_write(ring_buffer * buf, void * src);
